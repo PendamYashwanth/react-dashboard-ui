726f1bccf9ee51cd71c0880cd20e90357d466793
PendamYashwanth <pendamyashwanth769@gmail.com>
2024-04-25 10:35:13 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside>Sidebar</aside>
+      <main>
+        <article>
+          <header>Header</header>
+          Library cards
+        </article>
+        <section className="content-container">
+          <section className="slick">React slick</section>
+          <section>Recent posts</section>
+        </section>
+      </main>
     </div>
   );
 }
